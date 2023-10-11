85a8d8f32f8665627b658aad1f18036602216dde
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-10-12 02:28:25 +0300

    created "CategoryController"

---
+++ PokemonApp/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonApp.Data;
+
+namespace PokemonApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly Mapper _mapper;
+
+        public CategoryController(DataContext context, Mapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+    }
+}

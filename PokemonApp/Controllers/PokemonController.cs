9a7aeb161dccb5b7c81ed47996d42e9a4d062a04
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-10-12 00:23:28 +0300

    created "PokemonController"

---
+++ PokemonApp/Controllers/PokemonController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokemonApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+    }
+}

1bac7ad0a9b2066549fef0447691957491716294
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-10-11 15:14:02 +0300

    setup the base of the "DbContext"

--- PokemonApp/Data/DataContext.cs
+++ PokemonApp/Data/DataContext.cs
@@ -1,6 +1,12 @@
-﻿namespace PokemonApp.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PokemonApp.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
     }
 }

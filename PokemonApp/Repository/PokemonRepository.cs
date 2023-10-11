422ef166219d017c0a5adf4ee4ceff9731171a6d
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-10-12 00:15:43 +0300

    added "DataContext" to "PokemonRepository"

--- PokemonApp/Repository/PokemonRepository.cs
+++ PokemonApp/Repository/PokemonRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace PokemonApp.Repository
+﻿using PokemonApp.Data;
+
+namespace PokemonApp.Repository
 {
     public class PokemonRepository
     {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
     }
 }

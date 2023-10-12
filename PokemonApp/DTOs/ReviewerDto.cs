f4a9c6c968da4854b7dee67993cab42841c2fae4
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-10-12 16:34:03 +0300

    implemented "Reviewer" Interface, Repositroy and Controller

---
+++ PokemonApp/DTOs/ReviewerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonApp.DTOs
+{
+    public class ReviewerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}

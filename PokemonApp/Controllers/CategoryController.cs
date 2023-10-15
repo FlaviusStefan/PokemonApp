76a33af2b6095febd6f96a16b53d1e85cbd0fb3e
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-10-15 15:20:52 +0300

    implemented "UpdateCategory" method in Interface,Repository and Controller

--- PokemonApp/Controllers/CategoryController.cs
+++ PokemonApp/Controllers/CategoryController.cs
@@ -68,25 +68,25 @@ namespace PokemonApp.Controllers
         [ProducesResponseType(400)]
         public IActionResult CreateCategory([FromBody] CategoryDto categoryCreate)
         {
-            if(categoryCreate == null)
+            if (categoryCreate == null)
                 return BadRequest(ModelState);
 
             var category = _categoryRepository.GetCategories()
                            .Where(c => c.Name.Trim().ToUpper() == categoryCreate.Name.TrimEnd().ToUpper())
                            .FirstOrDefault();
 
-            if(category != null)
+            if (category != null)
             {
                 ModelState.AddModelError("", "Category already exists");
                 return StatusCode(422, ModelState);
             }
 
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
             var categoryMap = _mapper.Map<Category>(categoryCreate);
 
-            if(!_categoryRepository.CreateCategory(categoryMap))
+            if (!_categoryRepository.CreateCategory(categoryMap))
             {
                 ModelState.AddModelError("", "Something went wrong while saving");
                 return StatusCode(500, ModelState);
@@ -95,6 +95,41 @@ namespace PokemonApp.Controllers
             return Ok("Succesful");
         }
 
+        [HttpPut("{categoryId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public IActionResult UpdateCategory(int categoryId, [FromBody]CategoryDto updatedCategory) 
+        {
+            if (updatedCategory == null)
+                return BadRequest(ModelState);
+
+            if (categoryId != updatedCategory.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!_categoryRepository.CategoryExists(categoryId))
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var categoryMap = _mapper.Map<Category>(updatedCategory);
+
+            if(!_categoryRepository.UpdateCategory(categoryMap))
+            {
+                ModelState.AddModelError("", "Something went wrong updating category!");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("Succesful");
+
+
+        }
 
     }
 }

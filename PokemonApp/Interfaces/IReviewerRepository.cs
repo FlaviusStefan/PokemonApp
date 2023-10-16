f8adafda8bdac07a8e3dca7511cbcf22612e10bb
FlaviusStefan <flaviusstefanungureanu@gmail.com>
2023-10-17 01:03:40 +0300

    implemented "UpdateReviewer" method in Interface,Repository and Controller

--- PokemonApp/Interfaces/IReviewerRepository.cs
+++ PokemonApp/Interfaces/IReviewerRepository.cs
@@ -11,6 +11,8 @@ namespace PokemonApp.Interfaces
         bool ReviewerExists(int reviewerId);
 
         bool CreateReviewer(Reviewer reviewer);
+
+        bool UpdateReviewer(Reviewer reviewer);
         bool Save();
 
     }
